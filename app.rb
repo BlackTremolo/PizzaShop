15badfeb30dfd4c2c69dd9522738e26d3e9dae3d
Mihser <mihsee436@gmail.com>
2025-07-23 22:42:26 +0800

    Add cart page

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ end
 
 post '/cart' do
 	orders_input = params[:orders]
-	@items = parse_orders_line orders_input
+	@items = parse_orders_line(orders_input)
 	
 	@items.each do |item|
 		item[0] = Product.find(item[0])
