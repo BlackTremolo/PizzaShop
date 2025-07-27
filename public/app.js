8ddc7461d2f580d15d9c05b5973aa6bf0e8b5151
Mihser <mihsee436@gmail.com>
2025-07-27 19:39:44 +0800

    Add cancel button

--- public/app.js
+++ public/app.js
@@ -1,3 +1,9 @@
+function cancel_order()
+{
+	alert('Order is delete');
+	return false;
+}
+
 function some()
 	{
 		var x = window.localStorage.getItem('aaa');
@@ -71,3 +77,5 @@ function update_orders_button()
 	var text = 'Cart (' + cart_get_number_of_items() + ')';
 	$('#orders_button').val(text);
 }
+
+
