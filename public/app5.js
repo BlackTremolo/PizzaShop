299aec9748be7fb87a1963f8b946a54820996f5d
Mihser <mihsee436@gmail.com>
2025-07-27 20:36:56 +0800

    Delete .js files

--- public/app5.js
+++
@@ -1,6 +0,0 @@
-function update_orders_input()
-{
-	var orders1 = cart_get_orders();
-	$("#orders_input").val(orders1);
-
-}
--- public/app6.js
+++
@@ -1,5 +0,0 @@
-function update_orders_button()
-{
-	var text = 'Cart (' + cart_get_number_of_items() + ')';
-	$('#orders_button').val(text);
-}
